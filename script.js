54affeb1372c3915999c47e0a6f193aff791e4ff
cwkrundong27414 <156264@apps.everettsd.org>
2021-01-30 19:36:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var box = document.getElementById("box");
+
+function shrink() {
+  box.style.width = "10px";
+  box.style.height = "10px";
+}
+
+function grow() {
+  box.style.width = "200px";
+  box.style.height = "200px";
+}
+
+function change() {
+  box.style.width = "200px";
+  box.style.height = "200px";
+}
